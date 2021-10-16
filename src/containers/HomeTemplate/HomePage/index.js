329c6f155ffaf3b8e028d3ada1e7402c599d07c5
tunght994 <83862286+tunght994@users.noreply.github.com>
2021-10-16 13:16:28 +0700

    thanhtung (#1)

--- src/containers/HomeTemplate/HomePage/index.js
+++ src/containers/HomeTemplate/HomePage/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Carousel from '../Carousel'
+import data from '../Carousel/data';
 
 export default function HomePage() {
     return (
         <div>
-            <h3>Home Page</h3>
+            <Carousel
+                data={data}
+                control={true}
+                auto={false}
+                timeOut={3000}
+            />
         </div>
     )
 }
