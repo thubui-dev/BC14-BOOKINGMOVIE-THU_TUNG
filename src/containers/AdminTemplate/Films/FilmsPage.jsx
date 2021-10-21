39ffbb196dc888feadb1b26feb8ca0968474d32a
thubui-dev <84485959+thubui-dev@users.noreply.github.com>
2021-10-21 20:05:15 +0700

    file4 (#8)

--- src/containers/AdminTemplate/Films/FilmsPage.jsx
+++ src/containers/AdminTemplate/Films/FilmsPage.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import "./films.css";
 
-export default function FilmsPage() {
-    return (
-        <div>
-            <h3>Film Page</h3>
-        </div>
-    )
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function FilmPage() {
+  return (
+		<>
+		<div className="buttonAdd">
+			<button className="mt-5">Thêm phim </button>
+		</div>
+		<div className="buttonSearch">
+			<input type="search" className="buttonSearch--input" placeholder="Search here"/>
+  		<button type="submit" className="buttonSearch--submit">Search</button>
+		</div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 10 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Mã phim</TableCell>
+            <TableCell align="right">Hình ảnh</TableCell>
+            <TableCell align="right">Tên phim</TableCell>
+            <TableCell align="right">Mô tả</TableCell>
+            <TableCell align="right">Hành động</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+		</>
+  );
 }
